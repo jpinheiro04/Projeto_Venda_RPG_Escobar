39de0bb6573df82472f2aaaa0576d3d60d17fb07
jpinheiro04 <56983238+jpinheiro04@users.noreply.github.com>
2024-10-01 22:09:05 -0300

    Add files via upload

---
+++ database/migrations/2024_10_02_010043_create_produtos__table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            $table->string("produto_nome");
+            $table->string("produto_desc");
+            $table->double("produto_valor");
+            $table->integer("produto_Qtd");
+            $table->boolean("produto_ativo")->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos_');
+    }
+};
